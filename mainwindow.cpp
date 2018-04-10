c59e7639f86cf45596ab2c02f0fd1f00d12e51fe
Dominik Orlinski <dorlinsk@mion.elka.pw.edu.pl>
2018-04-10 10:40:12 +0200

    drugie okienko

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,18 +6,15 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->label_2->setVisible(false);
 }
 
 MainWindow::~MainWindow()
 {
-    ui->label_2->setVisible(false);
     delete ui;
 }
 
 void MainWindow::on_pushButton_clicked()
 {
-    this->setVisible(false);
     ui->label_2->setVisible(true);
-    while(getchar()!=13){;}
-
 }
