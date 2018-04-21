32c1de47b7e1305cfa7d17f2c8785fbf067e126b
Dominik Orliński <orlinskidominik@wp.pl>
2018-04-21 14:55:38 +0200

    Add files via upload

---
+++ RED_7/gamewindow.cpp
@@ -0,0 +1,16 @@
+#include "gamewindow.h"
+#include "ui_gamewindow.h"
+
+
+GameWindow::GameWindow(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::GameWindow)
+{
+
+    ui->setupUi(this);
+}
+
+GameWindow::~GameWindow()
+{
+    delete ui;
+}
